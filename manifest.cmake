bee1efa579f02b4aaa5b7323ec610e8d74cf6536
Kenneth Lareau <kenneth.lareau@couchbase.com>
2018-03-12 12:14:28 -0700

    CBD-2377: Initial version of libcouchbase-build
    Change-Id: Iab600d1bceff0e19052b7692032834462f8c79aa
    Reviewed-on: http://review.couchbase.org/90770
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Kenneth Lareau <kenneth.lareau@couchbase.com>

---
+++ manifest.cmake
@@ -0,0 +1,8 @@
+#
+# Keep the list sorted alphabetically, and the platforms alphabetically.
+# The syntax is:
+#
+# DECLARE_DEP (name VERSION version-revision PLATFORMS platform1 platform2)
+#
+DECLARE_DEP (libevent VERSION 2.1.8-cb3 PLATFORMS centos6 centos7 macos ubuntu14.04 ubuntu16.04 windows_msvc2015)
+DECLARE_DEP (libuv VERSION 1.13.1-cb3 PLATFORMS centos6 centos7 macos ubuntu14.04 ubuntu16.04 windows_msvc2015)

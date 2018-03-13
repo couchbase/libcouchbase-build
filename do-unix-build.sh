8ddab37698894b4de72fd49b65e76119248a57c2
Kenneth Lareau <kenneth.lareau@couchbase.com>
2018-03-12 18:01:38 -0700

    CBD-2377: Fix build scripts for libcouchbase (to NOT download source)
    Change-Id: I674fbcdcb36c0141ab451aac71cc5e30262f258c
    Reviewed-on: http://review.couchbase.org/90829
    Reviewed-by: Kenneth Lareau <kenneth.lareau@couchbase.com>
    Tested-by: Kenneth Lareau <kenneth.lareau@couchbase.com>

--- do-unix-build.sh
+++ do-unix-build.sh
@@ -1,18 +1,4 @@
 #!/bin/bash -ex
 
-# Global define
-PRODUCT=${1}
-RELEASE=${2}
-VERSION=${3}
-BLD_NUM=${4}
-
-# Download source
-env
-echo "Downloading source..."
-curl -LO http://latestbuilds.service.couchbase.com/builds/latestbuilds/${PRODUCT}/${RELEASE}/${BLD_NUM}/${PRODUCT}-${VERSION}-${BLD_NUM}-source.tar.gz
-echo "Extracting source..."
-tar xzf ${PRODUCT}-${VERSION}-${BLD_NUM}-source.tar.gz
-
-# Perform build
 cd libcouchbase && mkdir build && cd build
 ../cmake/configure && make
--- do-windows-build.bat
+++ do-windows-build.bat
@@ -1,16 +1,3 @@
-PRODUCT=%1
-RELEASE=%2
-VERSION=%3
-BLD_NUM=%4
-
-rem Download source
-echo Downloading source...
-set FILENAME=%PRODUCT%-%VERSION%-%BLD_NUM%-source.tar.gz
-set SITE=http://latestbuilds.service.couchbase.com/builds/latestbuilds/%PRODUCT%/%RELEASE%/%BLD_NUM%/%FILENAME%
-powershell -command "& { (New-Object Net.WebClient).DownloadFile('%SITE%', '%FILENAME%') }" || goto error
-echo Extracting source...
-cmake -E tar xzf %FILENAME% || goto error
-
 cd libcouchbase
 mkdir build
 cd build

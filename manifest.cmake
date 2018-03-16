9e81e87dce10ff591949933120128ac7c51f1aa7
Kenneth Lareau <kenneth.lareau@couchbase.com>
2018-03-15 18:26:46 -0700

    Ensure OpenSSL cbdep is used for Windows
    Change-Id: Id3384705141e090898a44c9dd47b0accc8cc83d7
    Reviewed-on: http://review.couchbase.org/91050
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Kenneth Lareau <kenneth.lareau@couchbase.com>

--- manifest.cmake
+++ manifest.cmake
@@ -6,3 +6,4 @@
 #
 DECLARE_DEP (libevent VERSION 2.1.8-cb3 PLATFORMS centos6 centos7 debian8 macosx ubuntu14.04 ubuntu16.04 windows_msvc2015)
 DECLARE_DEP (libuv VERSION 1.13.1-cb3 PLATFORMS centos6 centos7 debian8 macosx ubuntu14.04 ubuntu16.04 windows_msvc2015)
+DECLARE_DEP (openssl VERSION 1.0.2k-cb2 PLATFORMS windows_msvc2015)

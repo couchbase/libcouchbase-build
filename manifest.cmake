d54fc74198466c6027fc6efcadb1c7f90faadbf0
Kenneth Lareau <kenneth.lareau@couchbase.com>
2018-04-05 17:29:12 -0700

    CBD-2434: Use new VS versions of libevent, libuv and OpenSSL for libcouchbase
    Change-Id: Iffd58caf8cec93e17f9f25225eb8384ef25692bb
    Reviewed-on: http://review.couchbase.org/92279
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Kenneth Lareau <kenneth.lareau@couchbase.com>

--- manifest.cmake
+++ manifest.cmake
@@ -4,6 +4,7 @@
 #
 # DECLARE_DEP (name VERSION version-revision PLATFORMS platform1 platform2)
 #
-DECLARE_DEP (libevent VERSION 2.1.8-cb3 PLATFORMS centos6 centos7 debian8 debian9 macosx ubuntu14.04 ubuntu16.04 windows_msvc2015)
-DECLARE_DEP (libuv VERSION 1.13.1-cb3 PLATFORMS centos6 centos7 debian8 debian9 macosx ubuntu14.04 ubuntu16.04 windows_msvc2015)
-DECLARE_DEP (openssl VERSION 1.0.2k-cb2 PLATFORMS windows_msvc2015)
+DECLARE_DEP (libevent VERSION 2.1.8-cb3 PLATFORMS centos6 centos7 debian8 debian9 macosx ubuntu14.04 ubuntu16.04 windows_msvc2012 windows_msvc2013 windows_msvc2015 windows_msvc2017)
+DECLARE_DEP (libuv VERSION 1.13.1-cb3 PLATFORMS centos6 centos7 debian8 debian9 macosx ubuntu14.04 ubuntu16.04)
+DECLARE_DEP (libuv VERSION 1.13.1-cb4 PLATFORMS windows_msvc2012 windows_msvc2013 windows_msvc2015 windows_msvc2017)
+DECLARE_DEP (openssl VERSION 1.0.2k-cb2 PLATFORMS windows_msvc2012 windows_msvc2013 windows_msvc2015 windows_msvc2017)
